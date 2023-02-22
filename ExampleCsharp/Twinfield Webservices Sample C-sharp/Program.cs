c5afa0e370ca2dad0eebf7a151bb907cca7985b5
Deshmukh, Ketan <Ketan.Deshmukh@wolterskluwer.com>
2023-02-22 15:07:09 +0530

    [MTN-27836] Add simple c# example solution

---
+++ ExampleCsharp/Twinfield Webservices Sample C-sharp/Program.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Twinfield_Webservices_Sample_C_sharp.TwinfieldClusterSession;
+using Header = Twinfield_Webservices_Sample_C_sharp.TwinfieldClusterSession.Header;
+using LogonAction = Twinfield_Webservices_Sample_C_sharp.TwinfieldSession.LogonAction;
+using LogonResult = Twinfield_Webservices_Sample_C_sharp.TwinfieldSession.LogonResult;
+using Session = Twinfield_Webservices_Sample_C_sharp.TwinfieldClusterSession.Session;
+
+namespace Twinfield_Webservices_Sample_C_sharp
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// Create the session.
+			var session = new TwinfieldSession.Session();
+			var clusterSession = new Session();
+
+			// Request the user, password and organization.
+			Console.WriteLine("Enter user name:");
+			var user = Console.ReadLine().ToUpper();
+			Console.WriteLine("Enter password:");
+			var password = Console.ReadLine();
+			Console.WriteLine("Enter organization:");
+			var organization = Console.ReadLine().ToUpper();
+
+			ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+
+			// Log on to the session.
+			var logonResult =
+				session.Logon(user, password, organization, out var nextAction, out var cluster);
+
+			ProcessLogonResult(logonResult, cluster, session, nextAction, clusterSession);
+
+			DisplayCompanies(cluster, session);
+
+			SelectCompany(cluster, clusterSession, session);
+
+			DisplayCustomers(cluster, session);
+
+			AbandonTwinfieldSession(cluster, clusterSession);
+		}
+
+		static void ProcessLogonResult(LogonResult logonResult, string cluster, TwinfieldSession.Session session,
+			LogonAction nextAction, Session clusterSession)
+		{
+			switch (logonResult)
+			{
+				case LogonResult.Ok:
+					// Check the cluster URL.
+					if (!string.IsNullOrEmpty(cluster))
+						session.Url = cluster + "/webservices/session.asmx";
+
+					var actionError = string.Empty;
+					while (actionError == string.Empty && nextAction != LogonAction.None)
+					{
+						switch (nextAction)
+						{
+							case LogonAction.SMSLogon:
+								// Send the code to the session user.
+								var timeout = clusterSession.SmsSendCode();
+								if (timeout > 0)
+								{
+									// Request the sms code.
+									Console.WriteLine(
+										"An SMS has been sent, please fill in the code within {0} seconds:",
+										timeout);
+									var code = Console.ReadLine();
+									// Logon with the received sms code.
+									var smsLogonResult =
+										clusterSession.SmsLogon(code, out _);
+									switch (smsLogonResult)
+									{
+										case SMSLogonResult.Invalid:
+											actionError = "SMS log-on invalid.";
+											break;
+										case SMSLogonResult.TimeOut:
+											actionError = "SMS code timed out.";
+											break;
+										case SMSLogonResult.Disabled:
+											actionError = "Log-on is disabled.";
+											break;
+									}
+								}
+								else
+									actionError = "SMS failed to send";
+
+								break;
+							case LogonAction.ChangePassword:
+								// Request the passwords.
+								Console.WriteLine("Current password:");
+								var currentPassword = Console.ReadLine();
+								Console.WriteLine("New password:");
+								var newPassword = Console.ReadLine();
+								// Change the password.
+
+								var changePasswordResult =
+									clusterSession.ChangePassword(currentPassword, newPassword, out _);
+								switch (changePasswordResult)
+								{
+									case ChangePasswordResult.Invalid:
+										actionError = "Current password is invalid.";
+										break;
+									case ChangePasswordResult.NotDifferent:
+										actionError = "Passwords are not different.";
+										break;
+									case ChangePasswordResult.NotSecure:
+										actionError = "New password is not secure.";
+										break;
+									case ChangePasswordResult.Disabled:
+										actionError = "Log-on is disabled.";
+										break;
+								}
+
+								break;
+						}
+					}
+
+					if (actionError != string.Empty)
+					{
+						Console.WriteLine(actionError);
+						Console.WriteLine("Press any key to exit...");
+						Console.ReadKey();
+						Environment.Exit(0);
+					}
+					else
+						Console.WriteLine("Log-on successful.");
+
+					break;
+				case LogonResult.Blocked:
+					Console.WriteLine("Log-on is blocked.");
+					Console.ReadKey();
+					break;
+				case LogonResult.Untrusted:
+					Console.WriteLine("Log-on is untrusted.");
+					Console.ReadKey();
+					break;
+				case LogonResult.Invalid:
+					Console.WriteLine("Log-on is invalid.");
+					Console.WriteLine("Press any key to exit...");
+					Console.ReadKey();
+					Environment.Exit(0);
+					break;
+				case LogonResult.Deleted:
+					Console.WriteLine("Log-on is deleted.");
+					Console.ReadKey();
+					break;
+				case LogonResult.Disabled:
+					Console.WriteLine("Log-on is disabled.");
+					Console.ReadKey();
+					break;
+				case LogonResult.OrganisationInactive:
+					Console.WriteLine("Organization is inactive.");
+					Console.ReadKey();
+					break;
+				default:
+					Console.WriteLine("Unknown log-on result.");
+					Console.ReadKey();
+					break;
+			}
+		}
+
+		static void DisplayCompanies(string cluster, TwinfieldSession.Session session)
+		{
+			if (cluster == string.Empty) return;
+
+			// Create instance of process xml web service.
+			var processXml = new TwinfieldProcessXml.ProcessXml
+			{
+				// Create and assign the header.
+				HeaderValue = new TwinfieldProcessXml.Header
+				{
+					SessionID = session.HeaderValue.SessionID
+				}
+			};
+
+			// fill the request with xml as string
+			const string xmlRequest = "<list><type>offices</type></list>";
+
+			// Set url
+			processXml.Url = cluster + "/webservices/processxml.asmx";
+
+			Console.WriteLine("ProcessXml: Displaying a list of offices... ");
+			// ProcessXml as String
+			var xmlResult = processXml.ProcessXmlString(xmlRequest);
+			Console.WriteLine(xmlResult);
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+		}
+
+		static void SelectCompany(string cluster, Session clusterSession, TwinfieldSession.Session session)
+		{
+			if (cluster == string.Empty) return;
+
+			clusterSession.Url = cluster + "/webservices/session.asmx";
+
+			// Set sessionID header
+			clusterSession.HeaderValue = new Header
+			{
+				SessionID = session.HeaderValue.SessionID
+			};
+
+			Console.WriteLine("Enter company:");
+			var company = Console.ReadLine().ToUpper();
+			clusterSession.SelectCompany(company);
+		}
+
+		static void DisplayCustomers(string cluster, TwinfieldSession.Session session)
+		{
+			if (cluster == string.Empty) return;
+
+			// Create instance of finder webservice
+			var finder = new TwinfieldFinder.Finder();
+			finder.Url = cluster + "/webservices/finder.asmx";
+
+			// Set sessionID header
+			finder.HeaderValue = new TwinfieldFinder.Header
+			{
+				SessionID = session.HeaderValue.SessionID
+			};
+
+			// Build options array
+			var options = new string[2][];
+			options[0] = new[] { "dimtype", "DEB" };
+			options[1] = new[] { "section", "financials" };
+
+			// Search dimensions of type "DEB" with bank account which starts with "1*", return the first 10 matches.
+			Console.WriteLine("Displaying first 10 dimensions of type 'DEB' with bank account which starts with '1 * '");
+			var messages = finder.Search("DIM", "1*", 3, 1, 10, options, out var results);
+			if (messages.Any())
+				foreach (var message in messages)
+					Console.WriteLine(message.Text);
+			else
+				// Print results
+				if (results.Items != null)
+					for (var i = 0; i < results.Items.Length; ++i)
+						Console.WriteLine("{0}: Customer {1} ({2})", i, results.Items[i][0],
+							results.Items[i][1]);
+				else
+					Console.WriteLine("Nothing found.");
+
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+		}
+
+		static void AbandonTwinfieldSession(string cluster, Session clusterSession)
+		{
+			if (cluster == string.Empty) return;
+
+			// Abandon Twinfield session
+			clusterSession.Abandon();
+
+			Console.WriteLine("Twinfield session terminated.");
+			Console.WriteLine("Press any key to continue...");
+			Console.ReadKey();
+		}
+	}
+}
